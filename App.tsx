dce0b03306f0b759b09e5e6d2d7d1ad9e443a998
Fizzlefry <jonmikrut.jm@gmail.com>
2025-04-23 15:23:07 -0500

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,13 +4,17 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Aetherlink Lite</h1>
+      <h1>Aetherlink Dev Grid</h1>
       <ul>
         <li><a href="living-archive.html">Living Archive</a></li>
         <li><a href="avatar-builder.html">Avatar Builder</a></li>
         <li><a href="ritual-engine.html">Ritual Engine</a></li>
         <li><a href="quantum-forum.html">Quantum Forum</a></li>
-        <li><a href="dashboard.tsx">Mobile Dashboard</a></li> {/* New button */}
+        <li><a href="dashboard.html">Mobile Dashboard</a></li>
+        <li><a href="codecreator.html">Code Creator</a></li>
+        <li><a href="projectmanager.html">Project Manager</a></li>
+        <li><a href="templatemodule.html">Template Loader</a></li>
+        <li><a href="voice-activation.html">Create with Voice</a></li>
       </ul>
     </div>
   );
