1902ba3e621079ed126d3a6ab746b4a160d5e807
Fizzlefry <jonmikrut.jm@gmail.com>
2025-04-23 14:35:46 -0500

    Add files via upload

---
+++ Updated_App.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aetherlink Lite</h1>
+      <ul>
+        <li><a href="living-archive.html">Living Archive</a></li>
+        <li><a href="avatar-builder.html">Avatar Builder</a></li>
+        <li><a href="ritual-engine.html">Ritual Engine</a></li>
+        <li><a href="quantum-forum.html">Quantum Forum</a></li>
+        <li><a href="dashboard.tsx">Mobile Dashboard</a></li> {/* New button */}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
