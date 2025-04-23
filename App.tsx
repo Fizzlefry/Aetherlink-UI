4f08ed456b1889857e7f6009d539ad98a1233625
Fizzlefry <jonmikrut.jm@gmail.com>
2025-04-23 15:53:19 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-
-
 <li><a href="codecreator.html">Code Creator</a></li>
 <li><a href="projectmanager.html">Project Manager</a></li>
 <li><a href="templatemodule.html">Template Module</a></li>
 <li><a href="voice-activation.html">Voice Activation</a></li>
+<li><a href="askaether.html">Ask Aether (AI Assistant)</a></li>
+<li><a href="saveproject.html">Save My Project</a></li>
+<li><a href="exportbundle.html">Export Project Bundle</a></li>
