3d293ddce3889f7f6121e7264bc303e63f5f9a12
Fizzlefry <jonmikrut.jm@gmail.com>
2025-04-23 15:28:24 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,23 +1,6 @@
 
-import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Aetherlink Dev Grid</h1>
-      <ul>
-        <li><a href="living-archive.html">Living Archive</a></li>
-        <li><a href="avatar-builder.html">Avatar Builder</a></li>
-        <li><a href="ritual-engine.html">Ritual Engine</a></li>
-        <li><a href="quantum-forum.html">Quantum Forum</a></li>
-        <li><a href="dashboard.html">Mobile Dashboard</a></li>
-        <li><a href="codecreator.html">Code Creator</a></li>
-        <li><a href="projectmanager.html">Project Manager</a></li>
-        <li><a href="templatemodule.html">Template Loader</a></li>
-        <li><a href="voice-activation.html">Create with Voice</a></li>
-      </ul>
-    </div>
-  );
-}
-
-export default App;
+<li><a href="codecreator.html">Code Creator</a></li>
+<li><a href="projectmanager.html">Project Manager</a></li>
+<li><a href="templatemodule.html">Template Module</a></li>
+<li><a href="voice-activation.html">Voice Activation</a></li>
