138d318767572811f354af81945fdbe6072fef87
Fizzlefry <jonmikrut.jm@gmail.com>
2025-04-23 17:21:39 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 <li><a href="codecreator.html">Code Creator</a></li>
 <li><a href="projectmanager.html">Project Manager</a></li>
 <li><a href="templatemodule.html">Template Module</a></li>
@@ -7,4 +8,4 @@
 <li><a href="exportbundle.html">Export Project Bundle</a></li>
 <li><a href="saveproject_galactic.html">Save My Project (Galactic)</a></li>
 <li><a href="CodeOracle.html">Code Oracle</a></li>
-<li><a href="InfinityStorageMapper.html">Infinity Storage Mapper</a></li> 
+<li><a href="InfinityStorageMapper.html">Infinity Storage Mapper</a></li>
